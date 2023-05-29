2ed67bfbd1966f4f661b43eac3edab4c3b167800
Diogo Henrique <diogohs110@gmail.com>
2023-05-29 19:36:49 -0300

    Create svg animation

--- src/js/script.js
+++ src/js/script.js
@@ -1,24 +1,11 @@
+// Toggle functionality
+
 const pricingInputs = document.querySelectorAll('input[name="pricing"]');
 const priceTags = document.querySelectorAll('.plan__price span');
 const monthlyPrices = [19.99, 24.99, 39.99];
 const annuallyPrices = [199.99, 249.99, 399.99];
 
-let currentSelectedInput = getSelectedInput();
-
-function getSelectedInput() {
-	for (let i = 0; i <= pricingInputs.length; i++) {
-		if (pricingInputs[i].checked === true) {
-			return pricingInputs[i].value;
-		}
-	}
-}
-
-pricingInputs.forEach((input) => {
-	input.addEventListener('change', () => {
-		currentSelectedInput = input.value;
-    swichValues();
-	});
-});
+let currentSelectedInput;
 
 function swichValues() {
   for (let i = 0; i < priceTags.length; i++) {
@@ -29,3 +16,25 @@ function swichValues() {
     }
   }
 }
+
+pricingInputs.forEach((input) => {
+	input.addEventListener('change', () => {
+		currentSelectedInput = input.value;
+    swichValues();
+	});
+});
+
+
+// Fade in animation
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    }
+  })
+})
+
+const plansElements = document.querySelectorAll('.plan');
+plansElements.forEach((el) => observer.observe(el));
+
